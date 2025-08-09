52aab23855a5bb7e36e487ec4dc3b6a7fad4aff4
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 01:29:38 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/dependencyinjection/main.go
@@ -0,0 +1,23 @@
+// cmd/dependencyinjection/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "dependencyinjection/internal/dependencyinjection"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := dependencyinjection.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
